810a4b6d7fb6145d1d1b09a8d58f4b6e15bf4c30
Gustav <gustav.rosn@gmail.com>
2016-02-16 10:33:30 +0100

    shell init

--- craps/shooter.c
+++ craps/shooter.c
@@ -20,8 +20,9 @@ int main(int argc, char *argv[])
 	int seed_rd_fd = STDIN_FILENO;
 	int score_wr_fd = STDOUT_FILENO;
 
-	if (argc == 2)
-		id = strtol(argv[1], NULL, 10);
+	if (argc == 2){
+	  id = strtol(argv[1], NULL, 10);
+	}
 
 	shooter(id, seed_rd_fd, score_wr_fd);
 	
