53c19e2562fbc9ecd65fa35a924173430ca4bcb6
Gustav <gustav.rosn@gmail.com>
2016-02-16 11:08:34 +0100

    init fork

--- craps/craps.c
+++ craps/craps.c
@@ -28,84 +28,99 @@
 
 int main(int argc, char *argv[])
 {
-	int i, seed;
+  printf("NUM_PLAYERS = %i \n", NUM_PLAYERS);
+  int i, seed;
 
-	// TODO 1: Un-comment the following variables to use them in the 
-	//         exec system call. Using the function sprintf and the arg1 
-	//         variable you can pass the id parameter to the children 
+  // TODO 1: Un-comment the following variables to use them in the 
+  //         exec system call. Using the function sprintf and the arg1 
+  //         variable you can pass the id parameter to the children 
 
-	char arg0[] = "./shooter"; 
-	char arg1[10]; 
-	char *args[] = {arg0, arg1, NULL};
+  char arg0[] = "./shooter"; 
+  char arg1[10]; 
+  char *args[] = {arg0, arg1, NULL};
 	
 
-	// TODO 2: Declare pipe variables
-	//         - Of which data type should they be?
-	//         - How many pipes are needed?
-	//         - Try to choose self-explanatory variable names, e.g. seedPipe, scorePipe
+  // TODO 2: Declare pipe variables
+  //         - Of which data type should they be?
+  //         - How many pipes are needed?
+  //         - Try to choose self-explanatory variable names, e.g. seedPipe, scorePipe
 	
 
-	// TODO 3: initialize the communication with the players, i.e. create the pipes
+  // TODO 3: initialize the communication with the players, i.e. create the pipes
 
-	for (i = 0; i < NUM_PLAYERS; i++) {
+  for (i = 0; i < NUM_PLAYERS; i++) {
 
-	}
-
-
-	// TODO 4: spawn/fork the processes that simulate the players
-	//         - check if players were successfully spawned
-	//         - is it needed to store the pid of the players? Which data structure to use for this?
-	//         - re-direct standard-inputs/-outputs of the players
-	//         - use execv to start the players
-	//         - pass arguments using args and sprintf
-
-	for (i = 0; i < NUM_PLAYERS; i++) {
-	}
+  }
 
 
-	seed = time(NULL);
+  // TODO 4: spawn/fork the processes that simulate the players
+  //         - check if players were successfully spawned
+  //         - is it needed to store the pid of the players? Which data structure to use for this?
+  //         - re-direct standard-inputs/-outputs of the players
+  //         - use execv to start the players
+  //         - pass arguments using args and sprintf
+
+  for (i = 0; i < NUM_PLAYERS; i++) {
+    int f = fork();
+    if (f == 0 ){
+      puts("I am a child.");
+      break;
+    }
+    else if (f>0){
+      printf("I am the parent who just spawned %i.\n", f);
+    }
+    else if (f==-1){
+      puts("Child could not be created.");
+    }
+    else {
+      puts("Unknown error.");
+    }
+  }
+
+  if (1) return 0;
+
+  seed = time(NULL);
 
 
-	for (i = 0; i < NUM_PLAYERS; i++) {
-		seed++;
-		// TODO 5: send the seed to the players (write using pipes)
-	}
+  for (i = 0; i < NUM_PLAYERS; i++) {
+    seed++;
+    // TODO 5: send the seed to the players (write using pipes)
+  }
 
 
-	// TODO 6: read the dice results from the players via pipes, find the winner
+  // TODO 6: read the dice results from the players via pipes, find the winner
 
-	for (i = 0; i < NUM_PLAYERS; i++) {
+  for (i = 0; i < NUM_PLAYERS; i++) {
 
-	}
-
-
-	printf("master: player %d WINS\n", winner);
+  }
 
 
-	// TODO 7: signal the winner
-	//         - which command do you use to send signals?
-	//         - you will need the pid of the winner
+  printf("master: player %d WINS\n", winner);
+
+
+  // TODO 7: signal the winner
+  //         - which command do you use to send signals?
+  //         - you will need the pid of the winner
 
 	
 
-	// TODO 8: signal all players the end of game
-	//         - you will need the pid of all the players
+  // TODO 8: signal all players the end of game
+  //         - you will need the pid of all the players
 
-	for (i = 0; i < NUM_PLAYERS; i++) {
+  for (i = 0; i < NUM_PLAYERS; i++) {
 
-	}
-
-
-	printf("master: the game ends\n");
+  }
 
 
-	// TODO 9: cleanup resources and exit with success
-	//         wait for all the players/children to exit
-	//         before game master exits 
+  printf("master: the game ends\n");
 
-	for (i = 0; i < NUM_PLAYERS; i++) {
-	  
-	}
 
-	return 0;
+  // TODO 9: cleanup resources and exit with success
+  //         wait for all the players/children to exit
+  //         before game master exits 
+
+  for (i = 0; i < NUM_PLAYERS; i++) {
+  }
+
+  return 0;
 }
